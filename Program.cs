e46c4f0fbf14e6bd092e756c3878b13317cc56f0
ihongui <fihongui@st.ug.edu.gh>
2025-07-31 13:48:53 +0000

    Inheritance and Method Overriding

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Inheritance and Method Overriding ===");
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        Console.WriteLine("---======================---");
+        animal.MakeSound();
+        Console.WriteLine("---======================---");
+        Console.WriteLine("---This is a dog Sound---");
+        dog.MakeSound();
+        Console.WriteLine("---======================---");
+        Console.WriteLine("---This is a cat Sound---");
+        cat.MakeSound();
+        Console.WriteLine("---======================---");
+
+    }
+}
